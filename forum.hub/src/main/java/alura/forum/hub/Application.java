7a51389d3666a99439486841379c324dfcf58ee9
mhbs3 <mhbs3@g5c19.cin.ufpe.br>
2025-01-20 06:54:56 -0300

    Challenger ForumHub: Initial files.

---
+++ forum.hub/src/main/java/alura/forum/hub/Application.java
@@ -0,0 +1,13 @@
+package alura.forum.hub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
